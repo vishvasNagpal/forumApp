f4d687d52271819344b833fc4269a0ca1b8a588e
vishvasNagpal <vishvas.nagpal@gmail.com>
2018-02-06 23:42:02 +0530

    Boilerplate

---
+++ App.js
@@ -0,0 +1,22 @@
+// @flow
+
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View>
+        <Text>
+          Welcome to React Native!
+        </Text>
+        <Text>
+          To get started, edit App.js
+        </Text>
+      </View>
+    );
+  }
+}
